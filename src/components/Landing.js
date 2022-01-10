ec8f397b0171c2099c30651b49f96bd0bb39401d
NathanYee412 <nyee@csu.fullerton.edu>
2022-01-09 23:10:56 -0800

    first commit

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+
+export default function Landing() {
+    
+    return(
+        <div className='h-14 bg-cover bg-gradient-to-r from-sky-500 to-indigo-500'>
+            <h1>Hello World! My Name is Nathan.</h1>
+        </div>
+    );
+}
