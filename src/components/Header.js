efa4f32eec552cda0b4662fde49445e236a4d969
Nathan Yee <nyee@csu.fullerton.edu>
2022-01-12 09:38:54 -0800

    changed props to take image url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import { Disclosure } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
